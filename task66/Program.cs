c5d61f2a11a34fbb47da63e1dec7061bfde5a6f0
Alexandr <sashkavoytov20@gmail.com>
2022-08-22 23:35:58 +0500

    выполнил ДЗ

--- task66/Program.cs
+++ task66/Program.cs
@@ -2,3 +2,28 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+Console.Clear();
+
+Console.Write("Введите число M: ");
+int numberFirst = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberSecond = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+int GetSumNumber(int m, int n)
+{
+    if (m == n) return sum = sum + m;
+    if (m != n && m <= n)
+    {
+        sum = n + GetSumNumber(m, n - 1);
+    }
+    else if (m != n && m >= n)
+    {
+        sum = n + GetSumNumber(m, n + 1);
+    }
+    return sum;
+}
+
+int result = GetSumNumber(numberFirst, numberSecond);
+Console.WriteLine(result);
--- task68/Program.cs
+++ task68/Program.cs
@@ -1,3 +1,23 @@
 ﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
 // m = 3, n = 2 -> A(m,n) = 29
+
+int akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if ((m > 0) && (n == 0))
+    {
+        return akkerman(m - 1, 1);
+    }
+    else if ((m > 0) && (n > 0))
+    {
+        return akkerman(m - 1, akkerman(m, n - 1));
+    }
+    else
+        return n + 1;
+}
+
+Console.WriteLine(akkerman(3, 2));
